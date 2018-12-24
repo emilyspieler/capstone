81cb40b4af0b312a478a852131ba013f2c165b9d
SpielDog <spieldog2@spieldogs-mbp.home>
2018-12-23 20:21:35 -0600

    heroku 2b

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const staticController = require("../controllers/staticController");
+const spaceController = require("../controllers/spaceController");
 
 router.get("/", staticController.index);
 
